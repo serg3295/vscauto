562c45fd3f0b7a2588ec6e415fd1896b67a92f83
serg3295 <serg3295@gmail.com>
2021-03-06 12:41:03 +0300

    Support Lua 5.3

--- .vscode/auto.ps1
+++ .vscode/auto.ps1
@@ -1,4 +1,20 @@
-$lfs_size = 0x040000  #change if needed
+# settings.
+$lfs_size = 0x040000
+$lfs_base = 0x084000
+$lfs_mapped = 0x40200000 + $lfs_base #0x4027e000
+
+$luaVer = "5.1" # "5.1" or "5.3"
+
+if ($luaVer -eq "5.3") {
+  $luacCross = "c:/Program` Files` `(x86`)/Lua/5.3/luac.cross.exe"
+}
+elseif ($luaVer -eq "5.1") {
+  $luacCross = "luac.cross.exe" # for Lua 5.1
+  }
+else {
+  Write-Warning("Incorrect Lua version: $luaVer")
+  break
+}
 
 switch ($args[0]) {
 #--- command - FTP: build LFS & upload ---
@@ -8,7 +24,7 @@ switch ($args[0]) {
         if($isfile) {
             Remove-Item -Path $fpath
         }
-        luac.cross.exe -o $args[1] -f -m $lfs_size -l $args[3] | Out-file $args[4]
+        & $luaccross -o $args[1] -f -m $lfs_size -l $args[3] | Out-file $args[4]
         $isFile = Test-Path $fpath
         if($isfile) {
             curl.exe -T $args[1] --config $args[2] --ftp-pasv --disable-epsv --progress-bar  --list-only
@@ -26,7 +42,7 @@ switch ($args[0]) {
         if($isfile) {
             Remove-Item -Path $fpath
         }
-        luac.cross.exe -o $args[2] -f -m $lfs_size -l $args[3] | Out-file $args[4]
+        & $luaccross -o $args[2] -f -m $lfs_size -l $args[3] | Out-file $args[4]
         $isFile = Test-Path $fpath
         if($isfile) {
             nodemcu-tool upload  $args[1] $args[2]
@@ -44,7 +60,7 @@ switch ($args[0]) {
         if($isfile) {
             Remove-Item -Path $fpath
         }
-        luac.cross.exe -o $args[2] -l $args[3] | Out-file $args[4]
+        & $luaccross -o $args[2] -l $args[3] | Out-file $args[4]
         $isFile = Test-Path $fpath
         if($isfile) {
             nodemcu-tool upload  $args[1] $args[2]
@@ -62,7 +78,7 @@ switch ($args[0]) {
         if($isfile) {
             Remove-Item -Path $fpath
         }
-        luac.cross.exe -o $args[2] -l $args[3] | Out-file $args[4]
+        & $luaccross -o $args[2] -l $args[3] | Out-file $args[4]
         $isFile = Test-Path $fpath
         if($isfile) {
         nodemcu-tool upload  $args[1] $args[2]
@@ -76,14 +92,12 @@ switch ($args[0]) {
 #--- command - COM: absolute LFS Build & upload ---
     5 {
         $fpath = $args[2]
-        $lfs_base = 0x7e000       #change if needed
-        $lfs_mapped = 0x4027e000  #change if needed
 
         $isFile = Test-Path $fpath
         if($isfile) {
             Remove-Item -Path $fpath
         }
-        luac.cross.exe -o $args[2] -f -m $lfs_size -a $lfs_mapped -l $args[3] | Out-file $args[4]
+        & $luaccross -o $args[2] -f -m $lfs_size -a $lfs_mapped -l $args[3] | Out-file $args[4]
         $isFile = Test-Path $fpath
         if($isfile) {
             esptool.py $args[1] write_flash $lfs_base $fpath
@@ -109,6 +123,16 @@ switch ($args[0]) {
             Write-Warning("LuaSrcDiet error")
         }
         break
+    }
+#--- command - Cross compiler ---
+    7 {
+      $fpath = $args[1]
+      $isFile = Test-Path $fpath
+      if($isfile) {
+          Remove-Item -Path $fpath
+      }
+      & $luaccross -o $args[1] -l $args[2] | Out-file $args[3]
+      break
     }
 }
 exit
